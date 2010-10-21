e3b9e9e04be4de27cc79bef6c660bde6c006e279
Ana Nelson <ana@ananelson.com>
2010-10-21 07:48:05 +0000

    Change pynliner to process_dict to allow support for sectioned documents.

--- handlers/pynliner_handler.py
+++ handlers/pynliner_handler.py
@@ -1,16 +1,24 @@
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
+
 from dexy.handler import DexyHandler
 from pynliner import Pynliner
 
 class PynlinerHandler(DexyHandler):
     ALIASES = ['pynliner', 'inlinecss']
 
-    def process_text(self, input_text):
+    def process_dict(self, input_dict):
         #self.artifact.load_input_artifacts()
         #print self.artifact.input_artifacts_dict.keys()
         #matches = [k for k in self.artifact.input_artifacts_dict.keys() if k.endswith(".css|dexy")]
         #k = matches[0]
         css = open("pastie.css", "r").read()
-
-        p = Pynliner()
-        p.from_string(input_text).with_cssString(css)
-        return p.run()
+        
+        output_dict = OrderedDict()
+        for k, v in input_dict.items():
+            p = Pynliner()
+            p.from_string(v).with_cssString(css)
+            output_dict[k] = p.run()
+        return output_dict
