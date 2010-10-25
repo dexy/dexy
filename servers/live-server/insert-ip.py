bbb2e647828d353313aa422586e9f0b5de908e9b
Ana Nelson <ana@ananelson.com>
2010-10-25 14:41:24 +0100

    Add python script to change ip address in server config.

---
+++ servers/live-server/insert-ip.py
@@ -0,0 +1,21 @@
+import subprocess
+import re
+
+
+f = open("server.conf.template", "r")
+config = f.read()
+
+proc = subprocess.Popen("ifconfig", stdout=subprocess.PIPE)
+ifconfig, stderr = proc.communicate()
+
+r = re.compile("inet addr\:\s*([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)")
+m = re.search(r, ifconfig)
+
+ip = m.groups(1)[0]
+print "detected ip address", ip
+
+out = open("server.conf", "w")
+out.write(config.replace("localhost", ip)
+out.close()
+
+
