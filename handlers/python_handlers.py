0095e4b36ffc80c5a1281a4544c421c8ee371e8f
Ana Nelson <ana@ananelson.com>
2010-10-28 08:34:12 +0100

    Improve header/footer handlers.

--- handlers/python_handlers.py
+++ handlers/python_handlers.py
@@ -6,12 +6,15 @@ class CopyHandler(DexyHandler):
     OUTPUT_EXTENSIONS = [".*"]
     ALIASES = ['cp']
     
-    # Just copy the file without trying to read the contents.
+    # Just copy the file without trying to read the contents. 
+    # Rather hacky, needs testing to figure out implications for caching.
     def process(self):
         self.artifact.auto_write_artifact = False
         shutil.copyfile(self.doc.name, self.artifact.filename())
 
 class JoinHandler(DexyHandler):
+    """ This handler is so people have to acknowledge when they pass sectioned
+    text through a filter which doesn't preserve sectioning."""
     INPUT_EXTENSIONS = [".*"]
     OUTPUT_EXTENSIONS = [".*"]
     ALIASES = ['join']
@@ -26,15 +29,21 @@ class FooterHandler(DexyHandler):
 
     def process_text(self, input_text):
         self.artifact.load_input_artifacts()
-        footer_key = "_footer%s|jinja" % self.artifact.ext
-        footer_keys = [k for k in self.artifact.input_artifacts_dict.keys() if k.find(footer_key) > 0]
+        footer_key = "_footer%s" % self.artifact.ext
+        footer_keys = []
+        for k in self.artifact.input_artifacts_dict.keys():
+            contains_footer = k.find(footer_key) > 0
+            contains_pyg = k.find('|pyg') > 0
+            if contains_footer and not contains_pyg:
+                footer_keys.append(k)
+
         if len(footer_keys) > 0:
             footer_key = footer_keys[0]
             footer_text = self.artifact.input_artifacts_dict[footer_key]['data']
         else:
             raise Exception("No file matching %s was found to work as a footer." % footer_key)
                             
-        return "%s\n%s" % (footer_text, input_text)
+        return "%s\n%s" % (input_text, footer_text)
 
 class HeaderHandler(DexyHandler):
     INPUT_EXTENSIONS = [".*"]
@@ -43,16 +52,22 @@ class HeaderHandler(DexyHandler):
 
     def process_text(self, input_text):
         self.artifact.load_input_artifacts()
-        header_key = "_header%s|jinja" % self.artifact.ext
-        header_keys = [k for k in self.artifact.input_artifacts_dict.keys() if k.find(header_key) > 0]
+        header_key = "_header%s" % self.artifact.ext
+        header_keys = []
+        for k in self.artifact.input_artifacts_dict.keys():
+            contains_header = k.find(header_key) > 0
+            contains_pyg = k.find('|pyg') > 0
+            if contains_header and not contains_pyg:
+                header_keys.append(k)
         if len(header_keys) > 0:
             header_key = header_keys[0]
             header_text = self.artifact.input_artifacts_dict[header_key]['data']
         else:
-            raise Exception("No file matchine %s was found to work as a header." % header_key)
+            raise Exception("No file matching %s was found to work as a header." % header_key)
                             
         return "%s\n%s" % (header_text, input_text)
 
+# TODO implement combined header/footer handler as a shortcut
 
 class HeadHandler(DexyHandler):
     ALIASES = ['head']
