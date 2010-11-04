9c3963ef21bf6a299d79492e36bfbea4dfb6ee3d
Ana Nelson <ana@ananelson.com>
2010-11-04 01:16:38 +0100

    Improve detection of correct cucumber .rb file.

--- handlers/cucumber_handler.py
+++ handlers/cucumber_handler.py
@@ -2,7 +2,7 @@ from dexy.handler import DexyHandler
 from dexy.utils import ansi_output_to_html
 
 import pexpect
-
+import re
 class CucumberHandler(DexyHandler):
     INPUT_EXTENSIONS = [".feature"]
     OUTPUT_EXTENSIONS = [".html", ".txt"]
@@ -11,8 +11,8 @@ class CucumberHandler(DexyHandler):
     def process(self):
         self.artifact.load_input_artifacts()
         keys = self.artifact.input_artifacts_dict.keys() 
-        rb_file = self.artifact.doc.name.replace(".feature", ".rb")
-        matches = [k for k in keys if rb_file.find(k)]
+        rb_file = self.artifact.doc.name.replace(".feature", "\.rb")
+        matches = [k for k in keys if re.match(re.compile("^%s" % rb_file), k)]
         
         if len(matches) == 0:
             err_msg = "no file matching %s was found in %s" % (rb_file, keys)
