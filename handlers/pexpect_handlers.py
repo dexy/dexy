f169ed5eec8c95ab87fd7c639cd3281347d49f22
Ana Nelson <ana@ananelson.com>
2011-02-11 15:12:01 +0000

    Remove stdout handlers from pexpect module, fix minor issues.

--- handlers/pexpect_handlers.py
+++ handlers/pexpect_handlers.py
@@ -83,59 +83,6 @@ class ProcessSectionwiseInteractiveHandler(DexyHandler):
 
          return output_dict
 
-### @export "stdout"
-class ProcessStdoutHandler(DexyHandler):
-    """
-    Intended for use with command line processes where your only interest is in
-    the contents of stdout.
-    """
-    EXECUTABLE = '/usr/bin/env python'
-    INPUT_EXTENSIONS = [".txt", ".py"]
-    OUTPUT_EXTENSIONS = [".txt"]
-    ALIASES = ['py', 'python', 'pyout']
-    
-    def process(self):
-        self.artifact.generate_workfile()
-        if self.artifact.doc.args.has_key('timeout'):
-            timeout = self.artifact.doc.args['timeout']
-        else:
-            timeout = None
-        command = "%s %s" % (self.EXECUTABLE, self.artifact.work_filename(False))
-        cla = self.artifact.command_line_args()
-        if cla:
-            command = "%s %s" % (command, cla)
-        self.log.debug(command)
-        output, exit_status = pexpect.run(command, withexitstatus = True,
-                                          timeout=timeout,
-                                          cwd=self.artifact.artifacts_dir)
-        self.artifact.data_dict['1'] = output
-        if exit_status != 0:
-            # TODO rework this - probably want to raise error + not write
-            # artifact, or have option to ignore errors.
-            self.log.warn("an error occurred:\n%s" % output)
-            self.artifact.dirty = True # not sure this has any effect, hashcode already generated?
-
-### @export "bash"
-class BashHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env bash'
-    INPUT_EXTENSIONS = [".sh", ".bash", ".txt"]
-    OUTPUT_EXTENSIONS = [".txt"]
-    ALIASES = ['bash']
-
-### @export "php"
-class PhpHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env php'
-    INPUT_EXTENSIONS = [".php"]
-    OUTPUT_EXTENSIONS = [".html", ".txt"]
-    ALIASES = ['php']
-
-### @export "escript"
-class EscriptHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env escript'
-    INPUT_EXTENSIONS = [".erl"]
-    OUTPUT_EXTENSIONS = [".txt"]
-    ALIASES = ['escript']
-
 ### @export "clojure"
 class ClojureInteractiveHandler(ProcessLinewiseInteractiveHandler):
     EXECUTABLE = 'java clojure.main'
@@ -154,56 +101,6 @@ class LuaHandler(ProcessSectionwiseInteractiveHandler):
     ALIASES = ['lua']
     PROMPT = '>'
     TRAILING_PROMPT = '>>'
-
-### @export "luaout"
-class LuaStdoutHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env lua'
-    VERSION = '/usr/bin/env lua -v'
-    INPUT_EXTENSIONS = ['.lua']
-    OUTPUT_EXTENSIONS = ['.txt']
-    ALIASES = ['luaout']
-
-### @export "redcloth"
-class RedclothHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env redcloth'
-    INPUT_EXTENSIONS = [".txt", ".textile"]
-    OUTPUT_EXTENSIONS = [".html"]
-    ALIASES = ['redcloth', 'textile']
-
-### @export "redclothl"
-class RedclothLatexHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env redcloth -o latex'
-    INPUT_EXTENSIONS = [".txt", ".textile"]
-    OUTPUT_EXTENSIONS = [".tex"]
-    ALIASES = ['redclothl', 'latextile']
-
-### @export "rst2html"
-class Rst2HtmlHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env rst2html.py'
-    INPUT_EXTENSIONS = [".rst", ".txt"]
-    OUTPUT_EXTENSIONS = [".html"]
-    ALIASES = ['rst2html']
-
-### @export "rst2latex"
-class Rst2LatexHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env rst2latex.py'
-    INPUT_EXTENSIONS = [".rst", ".txt"]
-    OUTPUT_EXTENSIONS = [".tex"]
-    ALIASES = ['rst2latex']
-
-### @export "rst2beamer"
-class Rst2BeamerHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env rst2beamer'
-    INPUT_EXTENSIONS = [".rst", ".txt"]
-    OUTPUT_EXTENSIONS = [".tex"]
-    ALIASES = ['rst2beamer']
-
-### @export "sloccount"
-class SloccountHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env sloccount'
-    INPUT_EXTENSIONS = [".*"]
-    OUTPUT_EXTENSIONS = [".txt"]
-    ALIASES = ['sloc', 'sloccount']
 
 ### @export "timing"
 class ProcessTimingHandler(DexyHandler):
@@ -424,11 +321,4 @@ class DotHandler(DexyHandler):
         self.log.info(command)
         ad = self.artifact.artifacts_dir
         self.artifact.stdout = pexpect.run(command, cwd=ad)
-
-### @export "rb"
-class RubyStdoutHandler(ProcessStdoutHandler):
-    EXECUTABLE = '/usr/bin/env ruby'
-    INPUT_EXTENSIONS = [".txt", ".rb"]
-    OUTPUT_EXTENSIONS = [".txt"]
-    ALIASES = ['rb']
 
