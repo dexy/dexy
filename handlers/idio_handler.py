b4a9c79fdccb5db114147a04312a1e3ba361ee3e
Ana Nelson <ana@ananelson.com>
2011-03-26 10:19:27 +0000

    Move idio into separate handler file.

---
+++ handlers/idio_handler.py
@@ -0,0 +1,34 @@
+from dexy.handler import DexyHandler
+from idiopidae.runtime import Composer
+import idiopidae.parser
+
+class IdioHandler(DexyHandler):
+    """
+    Apply idiopidae to split document into sections at ### @export
+    "section-name" comments.
+    """
+    INPUT_EXTENSIONS = [".*"]
+    OUTPUT_EXTENSIONS = [".html", ".tex", ".txt"]
+    ALIASES = ['idio', 'idiopidae']
+
+    def process_text_to_dict(self, input_text):
+        composer = Composer()
+        builder = idiopidae.parser.parse('Document', input_text + "\n\0")
+
+        name = "input_text%s" % self.ext
+        if self.ext == '.pycon':
+            lexer = PythonConsoleLexer()
+        else:
+            lexer = get_lexer_for_filename(name)
+
+        fn = self.artifact.filename()
+        formatter = get_formatter_for_filename(fn, linenos=False)
+
+        output_dict = OrderedDict()
+
+        for i, s in enumerate(builder.sections):
+            lines = builder.statements[i]['lines']
+            formatted_lines = composer.format(lines, lexer, formatter) 
+            output_dict[s] = formatted_lines
+
+        return output_dict
--- handlers/pyg_handler.py
+++ handlers/pyg_handler.py
@@ -1,18 +1,15 @@
-from dexy.handler import DexyHandler
-
 try:
     from collections import OrderedDict
 except ImportError:
     from ordereddict import OrderedDict
 
-from idiopidae.runtime import Composer
+from dexy.handler import DexyHandler
 from pygments import highlight
 from pygments.formatters import get_formatter_for_filename
 from pygments.lexers import get_lexer_for_filename
 from pygments.lexers.agile import PythonConsoleLexer
 from pygments.lexers.agile import RubyConsoleLexer
 from pygments.lexers.web import JavascriptLexer
-import idiopidae.parser
 
 class PygHandler(DexyHandler):
     """
@@ -41,35 +38,4 @@ class PygHandler(DexyHandler):
             except UnicodeEncodeError as e:
                 self.log.warn("error processing section %s of file %s" % (k, self.artifact.key))
                 raise e
-        return output_dict
-
-class IdioHandler(DexyHandler):
-    """
-    Apply idiopidae to split document into sections at ### @export
-    "section-name" comments.
-    """
-    INPUT_EXTENSIONS = [".*"]
-    OUTPUT_EXTENSIONS = [".html", ".tex", ".txt"]
-    ALIASES = ['idio', 'idiopidae']
-
-    def process_text_to_dict(self, input_text):
-        composer = Composer()
-        builder = idiopidae.parser.parse('Document', input_text + "\n\0")
-
-        name = "input_text%s" % self.ext
-        if self.ext == '.pycon':
-            lexer = PythonConsoleLexer()
-        else:
-            lexer = get_lexer_for_filename(name)
-
-        fn = self.artifact.filename()
-        formatter = get_formatter_for_filename(fn, linenos=False)
-
-        output_dict = OrderedDict()
-
-        for i, s in enumerate(builder.sections):
-            lines = builder.statements[i]['lines']
-            formatted_lines = composer.format(lines, lexer, formatter) 
-            output_dict[s] = formatted_lines
-
         return output_dict
