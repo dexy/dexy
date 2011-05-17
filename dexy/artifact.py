4f2cf7f416462ee08d942f025472e1fcfda45d18
Ana Nelson <ana@ananelson.com>
2011-05-17 23:15:12 +0100

    Ensure attributes 'final' and 'additional' are persisted to metadata. To fix bug where additional artifacts weren't being written to output/ when read from cache.

--- dexy/artifact.py
+++ dexy/artifact.py
@@ -12,7 +12,9 @@ class Artifact(object):
         'binary',
         'key',
         'ext',
-        'stdout'
+        'stdout',
+        'final',
+        'additional'
     ]
 
     HASH_WHITELIST = [
