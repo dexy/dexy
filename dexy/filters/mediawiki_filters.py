30466f525823c6f4e80661b98afa321d11a4b253
Ana Nelson <ana@ananelson.com>
2012-02-26 16:27:41 +0000

    Update mediawiki filter. Return url of page or uploaded file.

--- dexy/filters/mediawiki_filters.py
+++ dexy/filters/mediawiki_filters.py
@@ -13,6 +13,9 @@ class MediaWikiFilter(ApiFilter):
 
     @classmethod
     def login_token(klass, forcelogin = False):
+        """
+        Returns login token if we already have one. Otherwise logs in and saves token.
+        """
         if forcelogin or not hasattr(klass, '_login_token'):
             payload = {
                     'action' : 'login',
@@ -20,12 +23,17 @@ class MediaWikiFilter(ApiFilter):
                     'lgpassword' : klass.read_param('password'),
                     'format' : 'json'
                     }
+
+            # TODO persist lgtoken somewhere?
+
             result = requests.post(klass.read_url(), data=payload)
             result_json = json.loads(result.text)
             klass._login_token = result_json['login']['token']
             klass.cookies = result.cookies
+
+            # Activate login by posting lgtoken along with username + password
             payload['lgtoken'] = klass._login_token
-            print requests.post(klass.read_url(), data=payload, cookies=klass.cookies).text
+            requests.post(klass.read_url(), data=payload, cookies=klass.cookies)
 
         return klass._login_token
 
@@ -60,6 +68,30 @@ class MediaWikiFilter(ApiFilter):
         return result_json['query']['pages'][str(pageid)]['revisions'][0]['*']
 
     @classmethod
+    def find_page(klass, title):
+        payload = klass.default_params()
+        payload['action'] = 'query'
+        payload['prop'] = 'info'
+        payload['inprop'] = 'url'
+        payload['titles'] = [title]
+        result = requests.post(klass.read_url(), cookies=klass.cookies, data=payload)
+        if hasattr(klass, 'log'):
+            klass.log.debug(result.text)
+        result_json = json.loads(result.text)
+        num_pages = len(result_json['query']['pages'])
+
+        if num_pages == 0:
+            print "No page found for %s" % title
+        elif num_pages == 1:
+            return result_json['query']['pages'].values()[0]
+        else:
+            raise Exception("More than 1 page found with title %s" % title)
+
+    @classmethod
+    def docmd_find_page(klass, title):
+        print klass.find_page(title)
+
+    @classmethod
     def docmd_pages(klass):
         for page_id, page_info in klass.pages().iteritems():
             print "%s\t%s" % (page_id, page_info['title'])
@@ -73,28 +105,40 @@ class MediaWikiFilter(ApiFilter):
         payload = self.default_params()
 
         if self.artifact.input_ext in ['.html', '.txt', '.md']:
-            self.log.debug("Creating page for %s" % self.artifact.key)
-            # web page content
+            # Create a new page or update an existing page.
             payload['action'] = 'edit'
             payload['text'] = input_text
             payload['token'] = self.pages().values()[0]['edittoken']
             payload['title'] = document_config['title']
             result = requests.post(self.read_url(), cookies=self.cookies, data=payload)
-            print json.loads(result.text)['edit']['result']
+
+            self.log.debug(result.text)
+
+            page_info = self.find_page(document_config['title'])
+            if not page_info:
+                raise Exception("No page created for %s" % document_config['title'])
+            url = page_info['fullurl']
         else:
-            print "Uploading binary file for %s" % self.artifact.key
-            # binary or other file
+            # Upload a binary file.
+            filename = self.artifact.previous_long_canonical_filename
+            self.log.debug("Uploading binary file %s" % (filename))
+
             payload['action'] = 'upload'
             payload['token'] = self.pages().values()[0]['edittoken']
-            payload['filename'] = self.artifact.previous_long_canonical_filename
+            payload['filename'] = filename
             payload['ignorewarnings'] = True # Replace existing files of this name.
+            if document_config.has_key('comment'):
+                payload['comment'] = document_config['comment']
 
-            files = {
-                    'file' : (payload['filename'], open(self.artifact.previous_artifact_filepath, "rb"))
-                    }
-            print "posting", payload, "to", self.read_url()
-            print self.cookies
+            f = open(self.artifact.previous_artifact_filepath, "rb")
+            files = {'file' : (payload['filename'], f) }
             result = requests.post(self.read_url(), cookies = self.cookies, data=payload, files=files)
-            print "upload result", json.loads(result.text)
+            f.close()
 
-        return ""
+            self.log.debug(result.text)
+
+            result_json = json.loads(result.text)
+            if not result_json['upload']['result'] == 'Success':
+                raise Exception("An error occurred while uploading file to mediawiki.")
+            url = result_json['upload']['imageinfo']['url']
+        return url
