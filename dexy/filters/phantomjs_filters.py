f5c9f86e7f7b1759852d0995eec96308bc5b9d23
Ana Nelson <ana@ananelson.com>
2012-02-28 22:30:13 +0000

    Fine-tune phantomjs filter. Screenshots now saved in same path as script.

--- dexy/filters/phantomjs_filters.py
+++ dexy/filters/phantomjs_filters.py
@@ -6,10 +6,10 @@ import shutil
 
 class PhantomJsRenderJavascriptInteractiveFilter(SubprocessFilter):
     """
-    Filter which runs javascript code against a web page, capturing a
-    screenshot after every section is run.
+    Filter which runs javascript on a web page and screenshots the result.
     """
     ALIASES = ['phsecshot']
+    DEFAULT_IMAGE_EXT = '.png'
     EXECUTABLE = 'phantomjs'
     INPUT_EXTENSIONS = ['.js', '.txt']
     OUTPUT_EXTENSIONS = ['.json']
@@ -17,28 +17,57 @@ class PhantomJsRenderJavascriptInteractiveFilter(SubprocessFilter):
     BINARY = False
 
     def setup_cwd(self):
-        return os.path.join(self.artifact.artifacts_dir, self.artifact.hashstring)
+        adir = self.artifact.artifacts_dir
+        wdir = self.artifact.hashstring
+        relpath = os.path.dirname(self.artifact.name)
+        return os.path.join(adir, wdir, relpath)
+
+    def image_ext(self):
+        """
+        What format screenshot? Defaults to DEFAULT_IMAGE_EXT, can be overridden.
+        """
+        if self.arg_value('image-ext'):
+            ext = self.arg_value('image-ext')
+            if not ext.startswith('.'):
+                raise Exception("Please start your file extension with a .")
+            return ext
+        else:
+            return self.DEFAULT_IMAGE_EXT
+
+    def new_image_artifact_key(self, section_name):
+        file_prefix = os.path.splitext(self.artifact.name)[0]
+        ext = self.image_ext()
+        key = "%s--%s-screenshot%s" % (file_prefix, section_name, ext)
+        return key
+
+    def new_image_artifact(self, section_name):
+        key = self.new_image_artifact_key(section_name)
+        ext = self.image_ext()
+        self.log.debug("Creating new artifact with key %s and ext %s" % (key, ext))
+        return self.artifact.add_additional_artifact(key, ext)
 
     def script_js(self, url, data_dict):
         """
         Construct the javascript which will actually be run by phantomjs.
         """
+        page_screenshot_artifact = self.new_image_artifact('initial')
+        page_fn = page_screenshot_artifact.filename()
         js = """
         var page = new WebPage();
 
         var consoleObject = {};
 
         page.onConsoleMessage = function (msg) {
-           consoleObject[sectionName] += msg;
+           consoleObject[sectionName] += ("" + msg + "\\n");
         };
 
         page.open("%s", function (status) {
-        """ % url
+                page.render("%s");
+        """ % (url, page_fn)
 
         for section_name, code in data_dict.iteritems():
             if code and not re.match("^\s*$", code):
-                artifact_key = os.path.join(os.path.dirname(self.artifact.name), "%s-screenshot.png" % section_name)
-                page_screenshot_artifact = self.artifact.add_additional_artifact(artifact_key, ".png")
+                page_screenshot_artifact = self.new_image_artifact(section_name)
                 page_fn = page_screenshot_artifact.filename()
                 js += """
                 sectionName = "%(section_name)s";
@@ -60,16 +89,23 @@ class PhantomJsRenderJavascriptInteractiveFilter(SubprocessFilter):
 
     def process(self):
         self.artifact.create_temp_dir(populate=True)
-        url = self.artifact.args['phsecshot']['url']
 
-        js = self.script_js(url, self.artifact.input_data_dict)
+        if self.arg_value('url'):
+            js = self.script_js(self.arg_value('url'), self.artifact.input_data_dict)
+        elif self.arg_value('filename'):
+            js = self.script_js(self.arg_value('filename'), self.artifact.input_data_dict)
+        elif False:
+            # TODO if there is only 1 input with HTML extension, use that.
+            pass
+        else:
+            raise Exception("You must specify either the url of a web page or local filename of an input file.")
 
-        workfile = "script.js"
-        workfile_path = os.path.join(self.artifact.artifacts_dir, self.artifact.hashstring, workfile)
+        script_name = "script.js"
+        workfile_path = os.path.join(self.setup_cwd(), script_name)
         with open(workfile_path, "w") as f:
             f.write(js)
 
-        command = "phantomjs %s" % workfile
+        command = "phantomjs %s" % script_name
         proc, stdout = self.run_command(command, self.setup_env())
         self.handle_subprocess_proc_return(command, proc.returncode, stdout)
 
@@ -80,7 +116,7 @@ class PhantomJsRenderJavascriptInteractiveFilter(SubprocessFilter):
         # Collect any artifacts which were generated in the tempdir, that need
         # to be moved to their final locations.
         for i in self.artifact.inputs().values():
-            src = os.path.join(work_dir, i.filename())
+            src = os.path.join(self.setup_cwd(), i.filename())
             if (i.virtual or i.additional) and os.path.exists(src):
                 self.log.debug("Copying %s to %s (%s)" % (src, i.filepath(), i.key))
                 shutil.copy(src, i.filepath())
