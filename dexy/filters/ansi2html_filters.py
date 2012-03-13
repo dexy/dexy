765f005fd53d3742886b5834688aec7e73e07349
Ana Nelson <ana@ananelson.com>
2012-03-13 22:11:40 +0000

    Make full=False the default for ansi2html so we don't get <html> etc. Add a filter command to generate CSS.

--- dexy/filters/ansi2html_filters.py
+++ dexy/filters/ansi2html_filters.py
@@ -6,11 +6,16 @@ class Ansi2HtmlFilter(DexyFilter):
     ALIASES = ['ansi2html']
     OUTPUT_EXTENSIONS = ['.html']
 
+    @classmethod
+    def docmd_css(klass):
+        converter = Ansi2HTMLConverter()
+        print converter.produce_headers()
+
     def process_dict(self, input_dict):
         converter = Ansi2HTMLConverter()
 
         output_dict = OrderedDict()
         for section_name, section_text in input_dict.iteritems():
-            output_dict[section_name] = converter.convert(section_text)
+            output_dict[section_name] = converter.convert(section_text, full=False)
 
         return output_dict
