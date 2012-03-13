4769f89da29e0c94250c27a54225ab868b3201db
Ana Nelson <ana@ananelson.com>
2012-03-13 22:03:34 +0000

    Make ansi2html filter a section-based filter.

--- dexy/filters/ansi2html_filters.py
+++ dexy/filters/ansi2html_filters.py
@@ -1,9 +1,16 @@
 from ansi2html import Ansi2HTMLConverter
 from dexy.dexy_filter import DexyFilter
+from ordereddict import OrderedDict
 
 class Ansi2HtmlFilter(DexyFilter):
     ALIASES = ['ansi2html']
     OUTPUT_EXTENSIONS = ['.html']
 
-    def process_text(self, input_text):
-        return Ansi2HTMLConverter().convert(input_text)
+    def process_dict(self, input_dict):
+        converter = Ansi2HTMLConverter()
+
+        output_dict = OrderedDict()
+        for section_name, section_text in input_dict.iteritems():
+            output_dict[section_name] = converter.convert(section_text)
+
+        return output_dict
