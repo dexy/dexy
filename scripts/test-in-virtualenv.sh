d9f9a6c514c7977e818107104774df8f024e5f81
Ana Nelson <ana@ananelson.com>
2012-11-09 18:31:31 -0800

    Use git to copy only files in repo for test-in-virtualenv script.

--- scripts/test-in-virtualenv.sh
+++ scripts/test-in-virtualenv.sh
@@ -4,7 +4,7 @@ TEST_DIR="/tmp/test-dexy/$TIMESTAMP"
 echo "Running in $TEST_DIR"
 mkdir -p $TEST_DIR
 pushd $TEST_DIR
-rsync -r --exclude cover --exclude .git ~/dev/dexy $TEST_DIR
+git clone ~/dev/dexy $TEST_DIR/dexy
 virtualenv testenv
 source testenv/bin/activate
 cd dexy
