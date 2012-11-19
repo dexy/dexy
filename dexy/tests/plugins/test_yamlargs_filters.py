3841fd8d336ba4abbf81f985890fd639f1343a0d
Ana Nelson <ana@ananelson.com>
2012-11-18 18:06:48 -0800

    Finish renaming to yamlargs

--- dexy/tests/plugins/test_yamlargs_filters.py
+++ dexy/tests/plugins/test_yamlargs_filters.py
@@ -1,18 +1,18 @@
 from dexy.tests.utils import wrap
 from dexy.doc import Doc
 
-def test_yamlmeta_no_yaml():
+def test_yamlargs_no_yaml():
     with wrap() as wrapper:
-        doc = Doc("example.txt|yamlmeta",
+        doc = Doc("example.txt|yamlargs",
                 contents = "This is the content.",
                 wrapper=wrapper)
 
         wrapper.run_docs(doc)
         assert doc.output().as_text() == "This is the content."
 
-def test_yamlmeta():
+def test_yamlargs():
     with wrap() as wrapper:
-        doc = Doc("example.txt|yamlmeta",
+        doc = Doc("example.txt|yamlargs",
                 contents = "title: My Title\n---\r\nThis is the content.",
                 wrapper=wrapper)
 
@@ -30,9 +30,9 @@ RESULT = """Here are the arguments you passed:
 bar: 42
 baz: xyz
 foo: 5"""
-def test_yamlmeta_filterargs():
+def test_yamlargs_filterargs():
     with wrap() as wrapper:
-        doc = Doc("example.txt|yamlmeta|filterargs",
+        doc = Doc("example.txt|yamlargs|filterargs",
                 contents = "%s\n---\r\nThis is the content." % YAML,
                 wrapper=wrapper)
 
