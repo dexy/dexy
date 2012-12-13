3a65d557dab206dc00d5665162629db9047b8dd0
Ana Nelson <ana@ananelson.com>
2012-12-13 12:00:01 -0800

    Remove old docs. Update release script.

--- docs/filters/filters.py
+++
@@ -1,5 +0,0 @@
-import dexy.plugins
-from dexy.filter import Filter
-
-for k in sorted(Filter.aliases):
-    print "%s %s" % (k, Filter.aliases[k].__name__)
---
+++ scripts/release-dexy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ev
+
+TIMESTAMP=`date +%s`
+TEST_DIR="/tmp/test-dexy/$TIMESTAMP"
+
+echo "Running test script in $TEST_DIR"
+mkdir -p $TEST_DIR
+pushd $TEST_DIR
+
+git clone ~/dev/dexy $TEST_DIR/dexy
+cd dexy
+
+python setup.py register sdist --formats=gztar,zip upload
+
+cp dist/* ~/dev/dexy-site-new/external-dependencies/
+
+cd ..
+
+# Now set up virtualenv and test install the new dexy.
+
+mkdir test
+cd test
+
+virtualenv testenv
+source testenv/bin/activate
+pip install dexy
+dexy version
+dexy templates
