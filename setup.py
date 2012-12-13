18175280a4c07062bf4afbef790de69cc5f8043a
Ana Nelson <ana@ananelson.com>
2012-12-12 23:27:22 -0800

    Add chardet dependency.

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         include_package_data = True,
         install_requires = [
             'PyYAML',
+            'chardet',
             'idiopidae>=0.5.4',
             'jinja2',
             'mock',
