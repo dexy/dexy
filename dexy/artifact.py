68f0348030624584b7f33e3f56e73457bdba15a9
Ana Nelson <ana@ananelson.com>
2012-12-16 22:19:54 -0800

    Fix bug where an artifact is run twice in a single run.

--- dexy/artifact.py
+++ dexy/artifact.py
@@ -258,7 +258,13 @@ class FilterArtifact(Artifact):
         self.elapsed = time.time() - start_time
 
     def load_cached_args(self):
-        rows = self.wrapper.db.task_from_previous_batch(self.hashstring)
+        rows = self.wrapper.db.task_from_current_batch(self.hashstring)
+        if len(rows) == 0:
+            rows = self.wrapper.db.task_from_previous_batch(self.hashstring)
+
+        if len(rows) == 0:
+            raise dexy.exceptions.InternalDexyProblem("No rows found for %s in current or previous batch." % self.hashstring)
+
         for k, v in json.loads(rows[0]['args']).iteritems():
             self.log.debug("updating arg %s of %s with stored value %s" % (k, self.key, v))
             self.args[k] = v
