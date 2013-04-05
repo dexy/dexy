60c6b9e3e54c662c0fe75087c5dec3b6f423e83e
Ana Nelson <ana@ananelson.com>
2013-04-05 12:45:44 -0700

    Add org mode filter.

--- dexy/filters/__init__.py
+++ dexy/filters/__init__.py
@@ -19,6 +19,7 @@ import dexy.filters.sub
 import dexy.filters.templating
 import dexy.filters.wordpress
 import dexy.filters.yamlargs
+import dexy.filters.org
 
 import dexy.filter
 import os
---
+++ dexy/filters/org.py
@@ -0,0 +1,38 @@
+from dexy.filters.process import SubprocessFilter
+import dexy.exceptions
+
+class OrgModeFilter(SubprocessFilter):
+    """
+    Convert .org files to other formats.
+    """
+    aliases = ['org']
+    _settings = {
+            'executable' : 'emacs',
+            'output' : True,
+            'input-extensions' : ['.org', '.txt'],
+            'output-extensions' : ['.txt', '.html', '.tex', '.pdf'],
+            'command-string' : """%(prog)s --batch %(args)s --eval "(progn \\
+(find-file \\"%(script_file)s\\") \\
+(%(export_command)s 1) \\
+(kill-buffer) \\
+)"
+"""
+            }
+
+    def command_string_args(self):
+        if self.ext == '.txt':
+            export_command = "org-export-as-ascii"
+        elif self.ext == '.html':
+            export_command = "org-export-as-html"
+        elif self.ext == '.tex':
+            export_command = "org-export-as-latex"
+        elif self.ext == '.pdf':
+            export_command = "org-export-as-pdf"
+        else:
+            msg = "unsupported extension %s"
+            msgargs = (self.ext)
+            raise dexy.exceptions.InternalDexyProblem(msg % msgargs)
+
+        args = self.default_command_string_args()
+        args['export_command'] = export_command
+        return args
