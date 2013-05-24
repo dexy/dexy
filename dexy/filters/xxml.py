9a29c6a40c1b0a4dec0f2e82392a182053dcc698
Ana Nelson <ana@ananelson.com>
2013-05-24 18:34:30 +0200

    Make xxml only active if lxml is installed.

--- dexy/filters/xxml.py
+++ dexy/filters/xxml.py
@@ -1,50 +1,76 @@
 from dexy.filter import DexyFilter
-from lxml import etree
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.formatters.latex import LatexFormatter
 from pygments.lexers import get_lexer_for_filename
 
+try:
+    from lxml import etree
+    AVAILABLE = True
+except ImportError:
+    AVAILABLE = False
+
 class XmlSectionFilter(DexyFilter):
     """
-    Stores all elements of an XML document in key-value storage. Elements are
-    indexed by an xpath (calculated using tree.getpath(element), and also an id
-    attribute if it is present in the element.
+    Stores all elements in the input XML document which have any of the attributes
+    specified in unique-attributes or qualified-attributes.
     """
     aliases = ["xxml", "xmlsec"]
     _settings = {
             'input-extensions' : [".xml", ".html", ".txt"],
+            'pygments' : ("Whether to apply pygments syntax highlighting", True),
+            'unique-attributes' : ("Elements to be added if they have this attribute, to be treated as globally unique.", ["id"]),
+            'qualified-attributes' : ("Elements to be added if they have this attribute, to be qualified by element type.", ["name"]),
             'output-data-type' : 'keyvalue',
             'output-extensions' :  [".json", ".sqlite3"]
             }
 
+    def is_active(self):
+        return AVAILABLE
+
+    def append_element_attributes_with_key(self, element, element_key):
+        source = etree.tostring(element, pretty_print=True).strip()
+        inner_html = "\n".join(etree.tostring(child) for child in element.iterchildren())
+        self.output_data.append("%s:lineno" % element_key, element.sourceline)
+        self.output_data.append("%s:tail" % element_key, element.tail)
+        self.output_data.append("%s:text" % element_key, element.text)
+        self.output_data.append("%s:tag" % element_key, element.tag)
+        self.output_data.append("%s:source" % element_key, source)
+        self.output_data.append("%s:inner-html" % element_key, inner_html)
+
+        if self.setting('pygments'):
+            self.output_data.append("%s:html-source" % element_key, highlight(source, self.lexer, self.html_formatter))
+            self.output_data.append("%s:latex-source" % element_key, highlight(source, self.lexer, self.latex_formatter))
+
     def process(self):
         assert self.output_data.state == 'ready'
 
-        lexer = get_lexer_for_filename(self.input_data.storage.data_file())
-        html_formatter = HtmlFormatter(lineanchors=self.output_data.web_safe_document_key())
-        latex_formatter = LatexFormatter()
+        if self.setting('pygments'):
+            self.lexer = get_lexer_for_filename(self.input_data.storage.data_file())
+            self.html_formatter = HtmlFormatter(lineanchors=self.output_data.web_safe_document_key())
+            self.latex_formatter = LatexFormatter()
 
-        root = etree.XML(str(self.input_data))
-        tree = root.getroottree()
+        if self.input_data.ext in ('.xml', '.txt'):
+            parser = etree.XMLParser()
+        elif self.input_data.ext == '.html':
+            parser = etree.HTMLParser()
+        else:
+            raise Exception("Unsupported extension %s" % self.input_data.ext)
+
+        tree = etree.parse(self.input_data.storage.data_file(), parser)
 
         for element in tree.iter("*"):
-            element_ids = [tree.getpath(element)]
-            if element.attrib.has_key('id'):
-                element_ids.append(element.attrib['id'])
-            if element.attrib.has_key('name'):
-                element_ids.append("%s:%s" % (element.tag, element.attrib['name']))
+            element_keys = []
+           
+            for attribute_name in self.setting('unique-attributes'):
+                if element.attrib.has_key(attribute_name):
+                    element_keys.append(element.attrib[attribute_name])
+            for attribute_name in self.setting('qualified-attributes'):
+                if element.attrib.has_key(attribute_name):
+                    element_keys.append(element.attrib[attribute_name])
+                    element_keys.append("%s:%s" % (element.tag, element.attrib[attribute_name]))
 
-            for element_id in element_ids:
-                source = etree.tostring(element, pretty_print=True).strip()
-                inner_html = "\n".join(etree.tostring(child) for child in element.iterchildren())
-                self.output_data.append("%s:lineno" % element_id, element.sourceline)
-                self.output_data.append("%s:tail" % element_id, element.tail)
-                self.output_data.append("%s:text" % element_id, element.text)
-                self.output_data.append("%s:tag" % element_id, element.tag)
-                self.output_data.append("%s:source" % element_id, source)
-                self.output_data.append("%s:inner-html" % element_id, inner_html)
-                self.output_data.append("%s:html-source" % element_id, highlight(source, lexer, html_formatter))
-                self.output_data.append("%s:latex-source" % element_id, highlight(source, lexer, latex_formatter))
+            for element_key in element_keys:
+                self.append_element_attributes_with_key(element, element_key)
 
         self.output_data.save()
