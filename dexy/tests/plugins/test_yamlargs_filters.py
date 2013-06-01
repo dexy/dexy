3cdbd10006dfdba4ea0858d6dff861c41dacd579
Ana Nelson <ana@ananelson.com>
2013-06-01 14:08:50 -0400

    Updates/fixes to yamlargs tests.

--- dexy/tests/plugins/test_yamlargs_filters.py
+++ dexy/tests/plugins/test_yamlargs_filters.py
@@ -13,7 +13,7 @@ def test_yamlargs_with_caching():
 
         task = wrapper.nodes["doc:example.txt|yamlargs"]
         assert task.output_data().title() == "My Title"
-        assert task.args_changed
+        assert task.state == 'ran'
 
         wrapper = Wrapper()
         doc = Doc("example.txt|yamlargs",
@@ -24,7 +24,18 @@ def test_yamlargs_with_caching():
         wrapper.run_docs(doc)
         task = wrapper.nodes["doc:example.txt|yamlargs"]
         assert task.output_data().title() == "My Title"
-        assert not task.args_changed
+        assert task.state == 'consolidated'
+
+        wrapper = Wrapper()
+        doc = Doc("example.txt|yamlargs",
+                wrapper,
+                [],
+                contents = "title: My Title\n---\r\nThis is the content."
+                )
+        wrapper.run_docs(doc)
+        task = wrapper.nodes["doc:example.txt|yamlargs"]
+        assert task.output_data().title() == "My Title"
+        assert task.state == 'consolidated'
 
 def test_yamlargs_no_yaml():
     with wrap() as wrapper:
@@ -55,6 +66,19 @@ YAML = """filterargs:
 
 def test_yamlargs_filterargs():
     with wrap() as wrapper:
+        doc = Doc("example.txt|yamlargs|filterargs",
+                wrapper,
+                [],
+                contents = "%s\n---\r\nThis is the content." % YAML,
+                )
+
+        wrapper.run_docs(doc)
+
+        output = doc.output_data().as_text()
+        assert "abc: xyz" in output
+        assert "foo: 5" in output
+
+        wrapper = Wrapper()
         doc = Doc("example.txt|yamlargs|filterargs",
                 wrapper,
                 [],
