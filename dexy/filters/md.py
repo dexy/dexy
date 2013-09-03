c67ea2b4aa068538bc513d5369eadc3f2c2515d7
Ana Nelson <ana@ananelson.com>
2013-09-03 12:00:23 -0700

    Add markdown slides filter.

--- dexy/filters/md.py
+++ dexy/filters/md.py
@@ -1,13 +1,8 @@
 from dexy.filter import DexyFilter
 import dexy.exceptions
-import logging
 import json
-
-try:
-    import markdown
-    AVAILABLE = True
-except ImportError as e:
-    AVAILABLE = False
+import logging
+import markdown
 
 class MarkdownFilter(DexyFilter):
     """
@@ -24,14 +19,11 @@ class MarkdownFilter(DexyFilter):
             'extensions' : ("Which Markdown extensions to enable.", { 'toc' : {} }),
             }
 
-    @classmethod
-    def is_active(klass):
-        return AVAILABLE
-
-    def process_text(self, input_text):
+    def capture_markdown_logger(self):
         markdown_logger = logging.getLogger('MARKDOWN')
         markdown_logger.addHandler(self.doc.wrapper.log.handlers[-1])
 
+    def initialize_markdown(self):
         extension_configs = self.setting('extensions')
         extensions = extension_configs.keys()
 
@@ -51,4 +43,52 @@ class MarkdownFilter(DexyFilter):
         except KeyError as e:
             raise dexy.exceptions.UserFeedback("Couldn't find a markdown extension option matching '%s'" % e.message)
 
+        return md
+
+    def process_text(self, input_text):
+        self.capture_markdown_logger()
+        md = self.initialize_markdown()
         return md.convert(input_text)
+
+class MarkdownSlidesFilter(MarkdownFilter):
+    """
+    Converts paragraphs to HTML and wrap each slide in a header and footer.
+    """
+    aliases = ['slides']
+
+    _settings = {
+            'extensions' : { 'nl2br' : {} },
+            'split' : (
+                "String to use to split slides.",
+                "\n\n\n" # e.g. 2 blank lines.
+                ),
+            'slide-header' : (
+                "Content to prepend to start of each slide.",
+                """<section class="slide">"""
+                ),
+            'slide-footer' : (
+                "Content to append to end of each slide.",
+                """</section>"""
+                ),
+            }
+
+    def process_text(self, input_text):
+        self.capture_markdown_logger()
+        md = self.initialize_markdown()
+
+        slides = ""
+        for counter, slide in enumerate(input_text.split(self.setting('split'))):
+            html = md.convert(slide)
+
+            # Variables to make available for string interpolation in header and footer.
+            interp = {
+                    'number' : (counter+1)
+                    }
+
+            header = self.setting('slide-header') % interp
+            footer = self.setting('slide-footer')% interp
+
+            slide_text = "\n%s\n%s\n%s\n" % (header, html, footer)
+            slides += slide_text
+
+        return slides
