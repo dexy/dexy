767080524567d0379e4aa8a36f423af5419c7498
Ana Nelson <ana@ananelson.com>
2013-10-26 19:27:28 -0700

    Switch to new setting('help') syntax.

--- dexy/commands/reporters.py
+++ dexy/commands/reporters.py
@@ -14,6 +14,6 @@ def reporters_command(
     
         print FMT % ('alias', 'default', 'info')
         for reporter in dexy.reporter.Reporter:
-            help_text = reporter.help().splitlines()[0]
+            help_text = reporter.setting('help').splitlines()[0]
             default_text = reporter.setting('default') and 'true' or 'false'
             print FMT % (reporter.alias, default_text, help_text)
