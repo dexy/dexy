90b848bddba3694216270adad7961b62c3401214
Ana Nelson <ana@ananelson.com>
2013-11-19 22:16:43 -0500

    Try using cashew to test pytest filter.

--- tests/plugins/test_pytest_filters.py
+++ tests/plugins/test_pytest_filters.py
@@ -7,14 +7,14 @@ def test_pytest_filter():
                 "modules.txt|pytest",
                 wrapper,
                 [],
-                contents="dexy_viewer"
+                contents="cashew"
                 )
         wrapper.run_docs(doc)
         data = doc.output_data()
 
-        testname = "test_viewer.basic_test"
+        testname = "test_cashew.test_standardize_alias_or_aliases"
         assert data[testname + ':doc'] == "docstring for test"
-        assert data[testname + ':name'] == "basic_test"
+        assert data[testname + ':name'] == "test_standardize_alias_or_aliases"
         assert data[testname + ':comments'] == "# comment before test\n"
         assert bool(data[testname + ':passed'])
-        assert "def basic_test():" in data[testname + ':source']
+        assert "def test_standardize_alias_or_aliases():" in data[testname + ':source']
