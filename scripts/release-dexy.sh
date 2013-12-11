92700309ff45d6569686b4ab514e2b66ef8de5cc
Ana Nelson <ana@ananelson.com>
2013-12-11 11:27:03 -0800

    Remove post-build testing, this is done in jenkins now.

--- scripts/release-dexy.sh
+++ scripts/release-dexy.sh
@@ -15,16 +15,3 @@ python setup.py register sdist --formats=gztar,zip upload
 # Copy .tgz .zip dexy packages to external dependencies directory
 cp dist/* ~/dev/dexy-site/external-dependencies/
 
-cd ..
-
-# Now set up virtualenv and test install the new dexy.
-
-mkdir test
-cd test
-
-virtualenv testenv
-source testenv/bin/activate
-pip install dexy
-pip install dexy-templates
-dexy version
-dexy templates
