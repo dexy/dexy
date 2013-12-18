b983bacf59013ad30dc1b6e23c77e718181fb0e4
Ana Nelson <ana@ananelson.com>
2013-12-18 11:14:06 -0800

    Move code which loads dexy plugins from other packages.

--- dexy/filters/__init__.py
+++ dexy/filters/__init__.py
@@ -38,13 +38,3 @@ import os
 
 yaml_file = os.path.join(os.path.dirname(__file__), 'filters.yaml')
 dexy.filter.Filter.register_plugins_from_yaml_file(yaml_file)
-
-# Automatically register plugins in any python package named like dexy_*
-import pkg_resources
-for dist in pkg_resources.working_set:
-    if dist.key.startswith("dexy-"):
-        import_pkg = dist.egg_name().split("-")[0]
-        try:
-            __import__(import_pkg)
-        except ImportError as e:
-            print "plugin", import_pkg, "not registered because", e
--- dexy/load_plugins.py
+++ dexy/load_plugins.py
@@ -2,3 +2,13 @@ import dexy.filters
 import dexy.reporters
 import dexy.parsers
 import dexy.datas
+
+# Automatically register plugins in any python package named like dexy_*
+import pkg_resources
+for dist in pkg_resources.working_set:
+    if dist.key.startswith("dexy-"):
+        import_pkg = dist.egg_name().split("-")[0]
+        try:
+            __import__(import_pkg)
+        except ImportError as e:
+            print "plugin", import_pkg, "not registered because", e
