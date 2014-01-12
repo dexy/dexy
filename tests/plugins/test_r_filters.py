ef7c66723b9b50c1f3363f3dd2dd0e77e6dd1e6f
Ana Nelson <ana@ananelson.com>
2014-01-12 11:25:51 -0500

    Add sweave filter.

---
+++ tests/plugins/test_r_filters.py
@@ -0,0 +1,20 @@
+from tests.utils import TEST_DATA_DIR
+from dexy.doc import Doc
+from tests.utils import wrap
+import os
+
+sweave_file = os.path.join(TEST_DATA_DIR, "example-2.Snw")
+
+def test_sweave_filter():
+    with open(sweave_file, 'r') as f:
+        sweave_content = f.read()
+
+    with wrap() as wrapper:
+        node = Doc("example.Snw|sweave",
+                wrapper,
+                [],
+                contents = sweave_content
+              )
+        wrapper.run_docs(node)
+        assert node.output_data().is_cached()
+        assert "Coefficients:" in unicode(node.output_data())
